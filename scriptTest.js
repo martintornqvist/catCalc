6836d89fd7927e73d18bc386a9a5726833996b94
martintornqvist <martintornqvist@Martins-MacBook-Pro-2.local>
2015-05-27 10:06:30 +0200

    Removed scriptTest.js

--- scriptTest.js
+++
@@ -1,7 +0,0 @@
-
-var heartInput = prompt('Ange i GRAM hur mycket hjärta du har');
-var liverInput = prompt('Ange i GRAM hur mycket lever du har');
-
-total = heartInput * liverInput;
-
-alert('Du har totalt: ' + total + ' gram kött att leka med');
